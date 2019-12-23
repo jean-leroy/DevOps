bb1e23c3469a2da91c17b6fa705be8555808849d
jean-leroy <jeanleroy.92500@gmail.com>
2019-12-23 03:18:12 +0100

    Basic menu

--- src/model/Calculator.java
+++ src/model/Calculator.java
@@ -2,4 +2,22 @@ package model;
 
 public class Calculator {
 
+	private double res;
+	
+	public void add(double... nb)
+	{
+		for ( double x : nb)
+		res += x;
+	}
+	
+	public void minus(double... nb)
+	{
+		for ( double x : nb)
+		res -= x;
+	}
+	
+	public double getResultat()
+	{
+		return res;
+	}
 }
--- src/model/Menu.java
+++ src/model/Menu.java
@@ -1,5 +1,33 @@
 package model;
 
+import java.util.Scanner;
+
 public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculator calc = new Calculator();
+
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Make your choice : '+' for add | '-' for minus' ");
+
+		char choice = scanner.next().charAt(0); 
+		
+		System.out.println("Enter the number");
+
+		double nb = scanner.nextDouble();
+		scanner.close();
+
+		switch (choice)
+		{
+		case '+' : 	calc.add(nb);
+		break;
+		case '-' : calc.minus(nb);
+		}
+		System.out.print(calc.getResultat());
+	}
 
 }
